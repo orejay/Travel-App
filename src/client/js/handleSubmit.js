db2ea4880558e4b9df44190eb0286a370360f2f0
Ore Jaiyesimi <orejay2017@gmail.com>
2022-05-24 21:48:49 +0100

    semi-final

--- src/client/js/handleSubmit.js
+++ src/client/js/handleSubmit.js
@@ -1,3 +1,4 @@
+//select html elements
 const subBtn = document.getElementById('sub-btn')
 const location = document.getElementById('location')
 const when = document.getElementById('when')
@@ -17,21 +18,27 @@ function submission (event) {
     event.preventDefault()
     let destination = document.getElementById('destination').value
     let userDate = document.getElementById('date').value
+    //get present date
     const todaysDate = new Date().getTime()
+    //get travel date
     const tripDate = new Date(userDate).getTime()
+    //calculate the number of days between present date and travel date
     const days = Math.round((tripDate - todaysDate)/86400000)
+    //get date in string format
     const date = new Date(userDate).toDateString()
     if (destination && userDate) {
         console.log("::: Form Submitted :::")
-        console.log(days)
+        //send user input to server
         postData('http://localhost:8080/check', {destination: destination, days: days, date: date})
         .then((resultData) =>{
                         updateUi(resultData)
                     });
     } else if (destination) {
+        //alert user if date is omitted
         dateErr.innerText = 'Please select a date'
         console.log('Please select a date')
     } else if (userDate) {
+        //alert user if destination is omitted
         destErr.innerText = 'Please enter your destination'
         console.log('Please enter your destination')
     } else {
@@ -40,6 +47,8 @@ function submission (event) {
     
 }
 
+
+//funtion to send the user entry to the server
 const postData = async(url="", data={}) => {
     const response = await fetch(url, {
         method: 'POST',
@@ -50,6 +59,7 @@ const postData = async(url="", data={}) => {
         body: JSON.stringify(data)
     })
     try{
+        //data received from the server
         const resultData = await response.json();
         console.log('Data received:', resultData)
         return resultData;
@@ -58,6 +68,7 @@ const postData = async(url="", data={}) => {
     }
 }
 
+//function to log the data on the UI
 const updateUi = async (resultData) => {
     console.log(resultData)
     if (dateErr.innerText || destErr.innerText) {
@@ -85,6 +96,7 @@ const updateUi = async (resultData) => {
 }
 
 export {
+    //export the submit function
     submission
 }
 
--- src/server/index.js
+++ src/server/index.js
@@ -6,12 +6,14 @@ const cors = require('cors')
 const dotenv = require('dotenv')
 dotenv.config()
 
+//declaring api credentials
 const geoUrl = process.env.GEON_URL
 const geoKey = process.env.GEON_KEY
 const wthrbit = process.env.WTHRBIT_URL
 const wthrbitKey = process.env.WTHRBIT_KEY
 const pxrbay = process.env.PXR_URL
 const pxrbayKey = process.env.PXR_KEY
+
 
 const app = express()
 app.use(express.json())
@@ -22,40 +24,52 @@ app.use(cors())
 app.use(express.static('dist'))
 
 app.get('/', function (req, res) {
+    //display bundled html as homepage
     res.sendFile(path.resolve('dist/index.html'))
 })
 
 app.get('/media/logo.png', function (req, res) {
+    //send logo to client side
     res.sendFile(path.resolve('./src/client/media/logo.png'))
 })
 
+//function to process request front client side
 app.post('/check', async function(req, res) {
+    //extract data from request body
     let destination = req.body.destination
     let days = req.body.days
     let date = req.body.date
+    //geonames api  call
     const geonUrl = `${geoUrl}=${destination}&maxRows=10&username=${geoKey}`
     const geoData = await fetch(geonUrl)
     const geonData = await geoData.json()
+    //extracting longitude and latitude from geonames response
     const lat = geonData.geonames[0].lat
     const lng = geonData.geonames[0].lng
+    //weatherbit api call using geonames data
     const wthrbitUrl = `${wthrbit}lat=${lat}&lon=${lng}&key=${wthrbitKey}`
     const wthrbitData = await fetch(wthrbitUrl)
     const weatherData = await wthrbitData.json()
+    //function call to get weather data
     getWeather(weatherData, days, pxrbay, pxrbayKey, destination, date)    
     .then((weather) => {
         const resultData = {
             weather: weather, 
         }
+        //sending data response to client side
         res.send(resultData)
     })
 })
 
+//function to extract weather data
 const getWeather = async (weatherData, days, pxrbay, pxrbayKey, destination, date) => {
         if (days <= 15 && days >= 0) {
+            //pixarbay api call
             const pxrbayUrl = `${pxrbay}?key=${pxrbayKey}&category=place&q=${destination}&image_type=photo}`
             const pxrbayData = await fetch(pxrbayUrl)
             const photoData = await pxrbayData.json()
             const image = photoData.hits[0].webformatURL
+            //create object to store weather data 
             const weather = { highTemp: weatherData.data[days].high_temp,
                 lowTemp: weatherData.data[days].low_temp,
                 icon: weatherData.data[days].weather.icon,
@@ -71,6 +85,7 @@ const getWeather = async (weatherData, days, pxrbay, pxrbayKey, destination, dat
             const pxrbayData = await fetch(pxrbayUrl)
             const photoData = await pxrbayData.json()
             const image = photoData.hits[0].webformatURL
+            //create object to store weather data
             const weather = { highTemp: weatherData.data[0].high_temp,
                 lowTemp: weatherData.data[0].low_temp,
                 icon: weatherData.data[0].weather.icon,
@@ -86,6 +101,7 @@ const getWeather = async (weatherData, days, pxrbay, pxrbayKey, destination, dat
             const pxrbayData = await fetch(pxrbayUrl)
             const photoData = await pxrbayData.json()
             const image = photoData.hits[0].webformatURL
+            //create object to store weather data
             const weather = { highTemp: weatherData.data[15].high_temp,
                 lowTemp: weatherData.data[15].low_temp,
                 icon: weatherData.data[15].weather.icon,
