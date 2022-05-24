d98bb971bac1505e4da2b3d5811d940f8911e29d
Ore Jaiyesimi <orejay2017@gmail.com>
2022-05-24 09:09:08 +0100

    wthrbit trial

--- src/client/js/handleSubmit.js
+++ src/client/js/handleSubmit.js
@@ -1,15 +1,51 @@
 const subBtn = document.getElementById('sub-btn')
 
-const submission = subBtn.addEventListener('click', function (event) {
+subBtn.addEventListener('click', submission)
+
+function submission (event) {
     event.preventDefault()
     let destination = document.getElementById('destination').value
-    console.log("::: Form Submitted :::")
-    postUrl('http://localhost:8080/destination', {url: destination})
-    .then((resultData) =>{
-                    updateUi(resultData)
-                });
-})
+    let userDate = document.getElementById('date').value
+    if (destination && userDate) {
+        console.log("::: Form Submitted :::")
+        const todaysDate = new Date().getTime
+        const tripDate = new Date(userDate).getTime
+        const days = Math.floor((tripDate - todaysDate)/86400000)
+        postData('http://localhost:8080/check', {destination: destination, days: days})
+        .then((resultData) =>{
+                        updateUi(resultData)
+                    });
+    } else if (destination) {
+        console.log('Please select a date')
+    } else if (userDate) {
+        console.log('Please enter your destination')
+    } else {
+        console.log('An error occurred please try again')
+    }
+    
+}
 
+const postData = async(url="", data={}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    try{
+        const resultData = await response.json();
+        console.log('Data received:', resultData)
+        return resultData;
+    }catch(error){
+        console.log('could not reach url', error)
+    }
+}
+
+const updateUi = async (resultData) => {
+    console.log(resultData)
+}
 
 export {
     submission
--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,15 @@ const fetch = require('node-fetch')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const dotenv = require('dotenv')
+const { json } = require('body-parser')
+dotenv.config()
+
+const geoUrl = process.env.GEON_URL
+const geoKey = process.env.GEON_KEY
+const wthrbit = process.env.WTHRBIT_URL
+const wthrbitKey = process.env.WTHRBIT_KEY
+const pxrbayUrl = process.env.PXR_URL
+const pxrbayKey = process.env.PXR_KEY
 
 const app = express()
 
@@ -19,6 +28,28 @@ app.get('/', function (req, res) {
 
 app.get('/logo/logo.png', function (req, res) {
     res.sendFile(path.resolve('./src/client/media/logo.png'))
+})
+
+app.post('/check', async function(req, res) {
+    let destination = req.body.destination
+    let days = req.body.days
+    const geonUrl = `${geoUrl}=${destination}&maxRows=10&username=${geoKey}`
+    fetch(geonUrl)
+    .then ((res) => res.json())
+    .then ((json) => {
+        const lat = json.geonData[0].lat
+        const lng = json.geonData[0].lng
+
+        const wthrbitUrl = `${wthrbit}lat=${lat}&lon=${lng}&key=${wthrbitKey}`
+        fetch(wthrbitUrl)
+        .then((res) => res.json())
+        .then((json) => {
+            console.log(json)
+        })
+
+    })
+
+    return 2
 })
 
 app.listen(8080, function () {
